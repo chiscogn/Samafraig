2cc35080dcff1fc5ab4088a6e9c65dcbef08abbe
chiscogn <132110992+chiscogn@users.noreply.github.com>
2024-11-28 22:40:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -168,4 +168,3 @@ sendKeepAliveRequest(); // Send an initial keep-alive request
 
 // Optional: Add loading element in your HTML
 // <div id="loading" style="display: none;">Loading questions...</div>
-

f77325713e365a83a4395c3d3b7119d2d8b42d67
chiscogn <132110992+chiscogn@users.noreply.github.com>
2024-11-29 11:23:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -64,20 +64,6 @@ const categoryNameElement = document.getElementById('categoryName');
 categoryNameElement.innerText = selectedCategory;
 
 // Function to update time
-function updateTime() {
-  const currentTimeElement = document.getElementById('currentTime');
-  const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  const seconds = now.getSeconds().toString().padStart(2, '0');
-  currentTimeElement.innerText = `${hours}:${minutes}:${seconds}`;
-}
-
-// Update the time every second
-setInterval(updateTime, 1000);
-
-// Initial time display
-updateTime();
 
 // Optional: Remove auto-reload during testing
 // Reload the page after 15 seconds for testing
